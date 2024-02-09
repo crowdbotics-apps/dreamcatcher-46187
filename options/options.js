3e653f4ac6fa4122a78a054c75b66592e5a0e390
Crowdbotics <team@crowdbotics.com>
2024-02-09 19:24:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dreamcatcher-46187",
+  url: "https://dreamcatcher-46187.botics.co",
+  api: "https://dreamcatcher-46187.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
